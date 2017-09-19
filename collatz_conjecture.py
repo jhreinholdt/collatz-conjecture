4215ed451ef5d63084a89f35ce1217bd556a6fcd
J H Reinholdt <30616675+jhreinholdt@users.noreply.github.com>
2017-09-19 15:20:37 +0200

    Add files via upload
    Updated code for plotting number of iterations for starting numbers up to 10E6 using matlibplot

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -7,17 +7,37 @@ Created on Mon Sep 18 16:03:24 2017
 #Collatz Conjecture - Start with a number n > 1. 
 #Find the number of steps it takes to reach one using the following process: 
 #If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1. 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 def collatzconjecture(number):
+    iterations = 0
     while number != 1:
         if number % 2 == 0:
             number = number / 2
+            iterations += 1
         else:
             number = 3 * number + 1
-    return int(number)
+            iterations += 1
+    return int(iterations)
             
 def main():
-    for number in range(2, 1000):
-        print(collatzconjecture(number))
+    maxno = 1000000
+    x = np.arange(2,maxno)
+    y = np.arange(0,maxno-2)
+    for number in x:
+        y[number-2] = collatzconjecture(number)
+        
+    dots = plt.plot(x,y)
+    plt.setp(dots, marker='.', linewidth='0.0', markersize='1.5')
+    plt.xscale('log')
+    plt.grid(True, which="both", ls="-", color='0.65')
+    plt.ylabel('Number of iterations')
+    plt.xlabel('Starting number')
+    plt.savefig('CollatzIterations.png', bbox_inches='tight')
+    plt.show()
+    
     
 if __name__ == '__main__':
     main()
